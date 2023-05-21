f6def6b298db2eb6993df42194e207b394c92c50
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-05-21 00:20:37 -0700

    Task 8: Print base16 numbers commit 1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	char y;
+
+	for (x = 0; x < 10; x++)
+		putchar((x % 10) + '0');
+
+	for (y = 'a'; y <= 'f'; y++)
+		putchar(y);
+
+	putchar('\n');
+
+	return (0);
+}
