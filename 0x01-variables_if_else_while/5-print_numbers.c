c03e598e8ce6f0d567ee72fc84884c9a8322ac70
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-05-20 23:56:56 -0700

    Task 5: Print numbers commit 1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main-Prints all digits from 1  to 10.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
