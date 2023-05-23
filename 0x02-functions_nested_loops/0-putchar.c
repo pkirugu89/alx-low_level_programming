82ad4c9b191948ac81d2267db67a1ddd59d94a4c
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-05-23 10:05:12 -0700

    Task 0: print putchar commit 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- writes _putchar followed by a new line.
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Putchar prototype  */
+int _putchar(char c);
+
+#endif
