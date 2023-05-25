6e5b6ef96f09eef143e9cd2e479f2ab9b33489ed
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-05-25 06:35:37 -0700

    Task 3: Print remaining days commit 1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,5 +6,9 @@
 void positive_or_negative(int i);
 /* Largest number */
 int largest_number(int a, int b, int c);
+/* Print remaining days */
+void print_remaining_days(int month, int day, int year);
+/* covert day */
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
