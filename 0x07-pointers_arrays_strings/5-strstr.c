c563f423a7fb0d825f1e69668fda3be352c1b3ac
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-06-06 01:12:23 -0700

    Task 5: locate substring commit 1

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: The string to be searched.
+ * @needle: The substring to be located.
+ *
+ * Return: If the substring is located - a pointer to the beginning
+ *                                       of the located substring.
+ *         If the substring is not located - NULL.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *x = haystack;
+		char *y = needle;
+
+		while (*x == *y && *y != '\0')
+		{
+			x++;
+			y++;
+		}
+		if (*y == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return  (0);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,5 +12,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 /* Task 4: Search a string of any set of byte prototype */
 char *_strpbrk(char *s, char *accept);
+/* Task 5: Locate a substring prototype */
+char *_strstr(char *haystack, char *needle);
 
 #endif
