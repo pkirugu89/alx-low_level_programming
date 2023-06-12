ee3a5b9a20d225e5162a3dd2fa1d8a5cf86fddb1
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-06-12 04:52:41 -0700

    Task 0: what is my name commit 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,8 +4,8 @@
 * main - prints the name of the program
 * @argc: number of arguments
 * @argv: array of arguments
-**
-Return: Always 0 (Success)
+*
+* Return: Always 0 (Success)
 */
 int main(int argc __attribute__((unused)), char *argv[])
 {
