b58967e86cece4458774922e1c65182df2fb3285
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-06-14 06:12:21 -0700

    Task 0: create a char array commit

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* Task 0: Create and array prototype */
+char *create_array(unsigned int size, char c);
+/* _putchar prototype */
+int _putchar(char c);
+/* Task 1: _strdup prototype */
+char *_strdup(char *str);
+/* Task 2: str_concat prototype */
+char *str_concat(char *s1, char *s2);
+/* Task 3: 2d integer array prototype */
+int **alloc_grid(int width, int height);
+/* Task 4: alloc_grid prototype */
+void free_grid(int **grid, int height);
+#endif
