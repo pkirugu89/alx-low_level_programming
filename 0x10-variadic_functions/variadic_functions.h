fc4566979710cffdb5a45bf5cc49bb0021b5e62b
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-06-23 10:15:19 -0700

    Task 0: print sum commit

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
+/* _putchar prototype */
+int _putchar(char c);
+/* Task 0: sum_them_all prototype */
+int sum_them_all(const unsigned int n, ...);
+/* Task 1: print_numbers prototype */
+void print_numbers(const char *separator, const unsigned int n, ...);
+/* Task 2: print_strings prototype */
+void print_strings(const char *separator, const unsigned int n, ...);
+/* Task 3: print_all prototype */
+void print_all(const char * const format, ...);
+#endif
