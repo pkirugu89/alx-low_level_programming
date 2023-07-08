301ca89c98fe80f7230829dbacf3b8119ec74e11
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-07-08 04:15:12 -0700

    Task 5: number of bits to be flipped commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the bit numbers changed
+ * from one number to another
+ * @n: 1st number
+ * @m: 2nd number
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int x, count = 0;
+	unsigned long int ct;
+	unsigned long int excl = n ^ m;
+
+	for (x = 63; x >= 0; x--)
+	{
+		ct = excl >> x;
+		if (ct & 1)
+			count++;
+	}
+
+	return (count);
+}
