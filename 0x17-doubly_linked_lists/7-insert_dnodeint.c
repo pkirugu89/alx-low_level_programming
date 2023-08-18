5ddb742f390175b33405374c823d67e863330c6b
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-18 15:02:11 +0000

    Task 7: insert a new node at given index commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,45 +5,48 @@
  * @h: start node
  * @idx: index to add node
  * @n: node number
+ *
  * Return: Always new_node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	/* Allocate memory for the new node */
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	dlistint_t *current;
-	unsigned int count = 0;
+	unsigned int i;
 
-	if (new_node == NULL)
+	/* Invalid node */
+	if (!h)
+		return (NULL);
+	/* Memory Allocation failed */
+	if (!new_node)
 		return (NULL);
 	new_node->n = n;
-	/* If list is empty and index is 0, the new node becomes the head */
-	if (*h == NULL && idx == 0)
-		new_node->prev = NULL;
-		new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	/* Insert at the beginning of the list */
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
 		*h = new_node;
-		return (new_node);
-	current = *h;
-	/* Transverse the list to find the desired index */
-	while (current != NULL && count < idx)
-		current = current->next;
-		count++;
-	/* Insert the new node at the end of the list */
-	if (current == NULL && count == idx)
-		new_node->prev = *h;
-		new_node->next = NULL;
-		if (*h != NULL)
-			(*h)->next = new_node;
-		return (new_node);
-	else if (current != NULL && count == idx)
-		/* Insert the new node before the current node */
-		new_node->prev = current->prev;
-		new_node->next = current;
-		if (current->prev != NULL)
-			current->prev->next = new_node;
-		current->prev = new_node;
-		return (new_node);
-	/* Index is out of bounds or new node can't be inserted */
-	free(new_node);
-	return (NULL);
+	}
+	else
+	{
+		current = *h;
+		for (i = 0; current && i < idx - 1; i++)
+			current = current->next;
+		/* checks if index is out of bound */
+		if (!current)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->next = current->next;
+		new_node->prev = current;
+		if (current->next)
+			current->next->prev = new_node;
+		current->next = new_node;
+	}
+	return (new_node);
 }
