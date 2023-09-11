d6b36c90493923f30d654bedda57123e69a9f3eb
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-09-11 18:53:27 +0000

    Task 6: delete hash table commit

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -47,6 +47,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 /* Print hash table prototype */
 void hash_table_print(const hash_table_t *ht);
-
+/* Delete an element from the hash table prototype. */
+void hash_table_delete(hash_table_t *ht);
 
 #endif
