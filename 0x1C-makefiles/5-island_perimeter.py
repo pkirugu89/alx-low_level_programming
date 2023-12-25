bc7d31e984e48a5fdeb81a500f81d383d215d322
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-12-25 18:07:25 +0000

    Task 6: 100-Makefile commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,16 +16,20 @@ def island_perimeter(grid):
 
     for i in range(len(grid)):
         for j in range(len(grid[0])):
+            print(f"Processing cell ({i}, {j}), value: {grid[i][j]}")
             if grid[i][j] == 1:
                 # Every land cell contributes 4 to the perimeter
                 perimeter += 4
+                print(f"Perimeter after adding 4: {perimeter}")
             # checks if there's a left island
             if j > 0 and grid[i][j - 1] == 1:
                 # Deduct 2 if the left neighbor is also land
                 perimeter -= 2
+                print(f"Perimeter after deducting 2: {perimeter}")
             # check if there's an island northwards (top)
             if i > 0 and grid[i - 1][j] == 1:
                 # Deduct 2 if the top neighbor is also land
                 perimeter -= 2
+                print(f"Perimeter after deducting 2: {perimeter}")
 
     return perimeter
