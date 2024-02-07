88e6485cccbf2e1568bcdb162250bd7286edf6c0
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-02-07 09:03:47 +0000

    Task 4 and 6 update 1 commit

---
+++ 0x1E-search_algorithms/tests/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/* C Library imports */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Task Prototypes */
+/* Task 0: Linear search prototype */
+int linear_search(int *array, size_t size, int value);
+/* Task 1: Binary search prototype */
+int binary_search(int *array, size_t size, int value);
+#endif
